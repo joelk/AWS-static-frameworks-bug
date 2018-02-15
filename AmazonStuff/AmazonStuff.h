b11376010c9015fb9f1aa91c9cec5cd46268b02e
Joel Kraut <joel.kraut@mindstronghealth.com>
2018-02-15 08:32:49 -0800

    Initial Commit

---
+++ AmazonStuff/AmazonStuff.h
@@ -0,0 +1,19 @@
+//
+//  AmazonStuff.h
+//  AmazonStuff
+//
+//  Created by Joel Kraut on 2/15/18.
+//  Copyright © 2018 Joel Kraut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AmazonStuff.
+FOUNDATION_EXPORT double AmazonStuffVersionNumber;
+
+//! Project version string for AmazonStuff.
+FOUNDATION_EXPORT const unsigned char AmazonStuffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmazonStuff/PublicHeader.h>
+
+
